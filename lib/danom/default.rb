7c080aebea8230cea2b307934cde810b2a172419
Uri Gorelik <uri.gore@gmail.com>
2017-12-01 11:41:12 -0500

    Added documentation for Default

--- lib/danom/default.rb
+++ lib/danom/default.rb
@@ -1,16 +1,43 @@
 require 'danom/monad'
 
 module Danom
+  # A default "monad". Not technically a monad as it take two values.
   class Default < Monad
+    # @param fallback [Object] The value on which to fallback if the doesn't
+    #   satisfy Just(value)
+    # @param value [Object]
     def initialize(fallback, value)
       @fallback = Just.new(fallback).value
       super(value)
     end
 
-    def value
+    # @override
+    def monad_value
       super || @fallback
     end
 
+    # Allows chaining on nil values with a fallback
+    #
+    # @note The fallback does not get the transformations
+    #
+    # @example
+    #   d = Default('Missing name', nil)
+    #   d = d.upcase.split.join #=> Danom::Default
+    #   name = ~d # 'Missing name'
+    #
+    #   d = Default('Missing name', 'Uri')
+    #   d = d.upcase #=> Danom::Default
+    #   name = ~d # 'URI'
+    #
+    # Default can be combined with a maybe. Extracting the value is recursive
+    # and will never return another monad.
+    #
+    # @example Combining with a Maybe
+    #   m = Maybe(nil)
+    #   d = Default('No name', m) #=> Danom::Default
+    #   name = ~d #=>  'No name'
+    #
+    # @see Danom::Monad#method_missing
     def and_then &block
       if @value == nil
         Default.new @fallback, nil
