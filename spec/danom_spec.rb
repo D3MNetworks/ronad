9ed151a760e6534073ef4020bfd5538d8552456a
Uri Gorelik <uri.gore@gmail.com>
2017-12-01 09:48:25 -0500

    Initial commit

---
+++ spec/danom_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Danom do
+  it "has a version number" do
+    expect(Danom::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
