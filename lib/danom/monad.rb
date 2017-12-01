bfb1ec468faf7a7aa015cc1f699e7117af5aa5eb
Uri Gorelik <uri.gore@gmail.com>
2017-12-01 11:21:02 -0500

    Added documentation to danom/monad

--- lib/danom/monad.rb
+++ lib/danom/monad.rb
@@ -1,4 +1,7 @@
 module Danom
+  # This class provides a common interface.
+  #
+  # @abstract
   class Monad
     def initialize(value)
       if Monad === value
@@ -8,19 +11,65 @@ module Danom
       end
     end
 
+    # Extract the value from the monad. If the underlying value responds to
+    # #value then this will be bypassed instead.
+    #
+    # @see #monad_value
+    # @return [Object] The unwrapped value
     def value
+      if !(Monad === @value) && @value.respond_to?(:value)
+        and_then{ |v| v.value }
+      else
+        monad_value
+      end
+    end
+
+
+    # Extract the value from the monad. Can also be extracted via `~` unary
+    # operator.
+    #
+    # @example
+    #   ~Maybe("hello") #=> "HELLO"
+    #   ~Maybe(nil)     #=> nil
+    #
+    # @note Useful when you want to extract the underlying value and it also
+    #   responds to #value
+    # @return [Object] The unwrapped value
+    def monad_value
       if Monad === @value
         @value.value
       else
         @value
       end
     end
+    alias_method :~@, :monad_value
 
-    # to_s does not get caught by method_missing
+
+    # Calls #to_s on the underlying value.
+    #
+    # @note This happens because method_missing does not override #to_s
+    # @private
     def to_s
       and_then { |v| v.to_s }
     end
 
+
+    private
+
+    # Provides convinience for chaining methods together while maintaining a
+    # monad.
+    #
+    # @example Chaining fetches on a hash that does not have the keys
+    #   m = Maybe({})
+    #   m_name = m[:person][:full_name] #=> Danom::Maybe
+    #   name = ~m_name #=> nil
+    #
+    # @example Safe navigation
+    #   m = Maybe(nil)
+    #   m_name = m.does.not.have.methods.upcase #=> Danom::Maybe
+    #   name = ~m_name #=> nil
+    #
+    # @return [Danom::Monad]
     def method_missing(method, *args, &block)
       unwrapped_args = args.map do |arg|
         case arg
